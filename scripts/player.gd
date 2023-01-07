8f69802ed31766995a05ae5bbeff4d0c3d31c4c9
benpm <schwalegos@gmail.com>
2023-01-06 20:59:17 -0700

    dreamcatcher thing with mouse

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,9 +10,11 @@ export(float) var gravity: float = 0.35
 export(float) var jumpvel: float = 6
 export(float) var fixrate: float = 1.0
 export(float) var attack_cooldown: float = 0.5
+export(float) var catcher_point_speed: float = 0.05
 
 onready var sprite: AnimatedSprite = $sprite
 onready var attack_area: Area2D = $attack_area
+onready var catcher_line: Line2D = $catcher_line_container/catcher_line
 
 onready var initpos: Vector2 = position
 var vel: Vector2 = Vector2(0, 0)
@@ -25,6 +27,8 @@ var dead := false
 var dream = 0
 var can_attack := true
 var attack_disable_nextframe := false
+var catcher_point := Vector2.ZERO
+var catching := false
 
 func add_dream(amount):
 	dream += amount
@@ -64,6 +68,26 @@ func _process(_delta: float) -> void:
 	# World boundary
 	# if position.y > maxy:
 	# 	death()
+
+	# Draw dreamcatcher path
+	if Input.is_action_pressed("action") or catching:
+		var mouse = get_global_mouse_position()
+		var pts = catcher_line.points.size()
+		if pts == 0:
+			catcher_point = mouse
+		if pts == 0 or catcher_point.distance_to(catcher_line.points[pts - 1]) > 10:
+			catcher_line.add_point(catcher_point)
+		if (pts > 5 and catcher_point.distance_to(catcher_line.points[0]) < 25) or catching:
+			catcher_point = lerp(catcher_point, catcher_line.points[0], catcher_point_speed)
+			catching = true
+			if catcher_point.distance_to(catcher_line.points[0]) < 1:
+				catcher_line.clear_points()
+				catching = false
+		else:
+			catcher_point = lerp(catcher_point, mouse, catcher_point_speed)
+	elif Input.is_action_just_released("action"):
+		if not catching:
+			catcher_line.clear_points()
 
 func _physics_process(_delta: float) -> void:
 
